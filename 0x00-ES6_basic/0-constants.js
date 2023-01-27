c0ce71c42dae7c0152c2bd3565837228af808377
Astewul <loloalex1992@gmail.com>
2023-01-28 01:21:27 +0300

    update files

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,12 +1,15 @@
-function taskFirst() { 
+export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
-}
-function getLast(){
-    return 'is okay';
-}
-function taskNext(){
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
+  
     return combination;
-}
+  }
