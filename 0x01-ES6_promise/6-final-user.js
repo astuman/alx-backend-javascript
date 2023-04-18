fcacdaa3a870bbdd7bc7eb45fb071838b96d3c0e
Astewul from webterm <loloalex1992@gmail.com>
2023-04-18 14:03:50 +0000

    commit task

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import {signUpUser} from './4-user-promise.js';
 import {uploadPhoto} from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default async function handleProfileSignup(firstName, lastName, filename) {
 	const user = {
 		status: 'pending',
 		value: ''
