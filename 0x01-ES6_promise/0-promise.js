ac39055852acf82553d3d6d6505b023da1468d81
Astewul from webterm <loloalex1992@gmail.com>
2023-04-18 13:13:52 +0000

    commit tasks

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,3 @@
-export default function getResponseFromAPI(success){
-	return new Promise((resolve, reject)=>{
-		if(success === true){
-			resolve({
-				status: 200,
-				body: 'Success',
-			});
-		}else{
-			reject(new Error('The fake API is not working currently'))
-		}
-	});
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
