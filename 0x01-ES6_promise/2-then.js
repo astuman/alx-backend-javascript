53f32889aecdd07cfc9e77190e55eedfb578a4d4
Astewul from webterm <loloalex1992@gmail.com>
2023-04-20 06:24:24 +0000

    commit task

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,8 +2,8 @@ export default function handleResponseFromAPI(promise){
 	return promise
 	.then(()=>({
 		status: 200,
-		body: 'Success',
+		body: 'success',
 	}))
-	.catch(Error)
+	.catch(()=>Error())
 	.finally(()=>console.warn('Got a response from the API'))
 }
