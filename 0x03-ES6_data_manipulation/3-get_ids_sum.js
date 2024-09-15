74d1e8ee53e9871c659ee3f6242c7e11834336a9
Astewul Alemu Derseh <101445231+astuman@users.noreply.github.com>
2024-09-15 22:12:45 +0300

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,9 @@
-export default function getStudentIdsSum(array) {
-  return array.reduce((accumulator, i) => (accumulator + i.id, 0));
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
 }
